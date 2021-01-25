827a3320c46def347f5817a78bc984431afdbb85
MerlionRock <merlionrock@yahoo.com.sg>
2021-01-26 00:10:35 +0800

    Quick Fix
    Additional information to help new users to identify what's wrong if they cannot start RAB

--- rab/run.py
+++ rab/run.py
@@ -1,2 +1,7 @@
-import sourcedefender
+import sys
+try:
+    import sourcedefender
+except:
+    print('Please check you have install requirements.txt successfully.\nRefer to step 10 in Github')
+    sys.exit(0)
 import rab
