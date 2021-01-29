e68b8f4681cd7bbea40b963beb2a7324c5db20ce
rushi12565 <74148058+rushi12565@users.noreply.github.com>
2021-01-29 11:29:08 +0530

    Add files via upload

---
+++ test2.ps1
@@ -0,0 +1,3 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+choco install visualstudio2019-workload-vctools -y
+PowerShell -NoExit "C:\RealAndroidBot"
