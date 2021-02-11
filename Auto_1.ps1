aba5a2b6b26166cb8fbdd480ef19ca6eb06aaddb
rushi12565 <74148058+rushi12565@users.noreply.github.com>
2021-02-11 18:27:23 +0530

    Delete Auto_1.ps1

--- Auto_1.ps1
+++
@@ -1,6 +0,0 @@
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
-choco install adb -y
-choco install tesseract --pre -y 
-choco install python -y
-choco install visualstudio2019buildtools -y
-Restart-Computer -Confirm
