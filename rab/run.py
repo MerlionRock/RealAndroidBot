c6a294788f9c463bf5af9164033943d17d48ab9e
MerlionRock <merlionrock@yahoo.com.sg>
2021-02-12 21:50:50 +0800

    Update run.py
    catch all errors

--- rab/run.py
+++ rab/run.py
@@ -1,7 +1,10 @@
 import sys
 try:
     import sourcedefender
-except:
-    print('Please check you have install requirements.txt successfully.\nRefer to step 10 in Github')
+except Exception as e:
+    print('Please check you have install requirements.txt successfully.\nRefer to step 10 in Github: {}'.format(e))
     sys.exit(0)
-import rab
+try:
+    import rab
+except Exception as e:
+    print("Encounter unexpected error: {}".format(e))
