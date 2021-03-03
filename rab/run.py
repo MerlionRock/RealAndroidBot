6e85b63dfaf000b0c57573eded01929fe2c933de
MerlionRock <merlionrock@yahoo.com.sg>
2021-03-03 13:40:55 +0800

    v1.09 Alpha
    1. RAB will try to avoid clicking on gyms
    2. Attempt to fix not getting Level
    3. Config.example is now in rab folder
    4. RAB will auto create config.yaml if not found
    5. GUI completed (almost)

--- rab/run.py
+++ rab/run.py
@@ -1,10 +1,2 @@
-import sys
-try:
-    import sourcedefender
-except Exception as e:
-    print('Please check you have install requirements.txt successfully.\nRefer to step 10 in Github: {}'.format(e))
-    sys.exit(0)
-try:
-    import rab
-except Exception as e:
-    print("Encounter unexpected error: {}".format(e))
+import sourcedefender
+import rabgui
